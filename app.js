409016b5a81c1e11a773a952ac0e5e6134f54668
GnobBarley <pat.calhoun97@gmail.com>
2019-07-05 09:03:00 -0500

    Added elements to the index.html and started creating functionality with Js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-//nothing to see here
+const gridContainer = document.querySelector("#gridContainer");
+
+let createDiv;
+let createGrid = makeGrid(32);
+//let gridCell = document.getElementsByClassName("gridCell");
+function makeGrid(gridSize){
+
+    gridContainer.style.gridTemplateColumns = `repeat(${gSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gSize}, 1fr )`;
+
+    for(let i =0; i < gridSize * gridSize; i++){
+        createDiv = document.createElement('div');
+        createDiv.classList.add('cell');
+        gridContainer.appendChild(createDiv);
+    }
+
+}
+function resetGrid(){
+
+}
+function colorPickerGrid(){
+
+}
+function randomColorGrid(){
+
+}
