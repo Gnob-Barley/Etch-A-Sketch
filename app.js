1b91e349f58af88e8645175c3b5f922f4125d1dd
GnobBarley <pat.calhoun97@gmail.com>
2019-07-05 11:07:39 -0500

    Added full functionality, styled the page, added a color picker and rainbow

--- app.js
+++ app.js
@@ -1,26 +1,68 @@
 const gridContainer = document.querySelector("#gridContainer");
 
 let createDiv;
-let createGrid = makeGrid(32);
-//let gridCell = document.getElementsByClassName("gridCell");
-function makeGrid(gridSize){
+let createGrid = makeGrid(5);
+let gridCell = document.getElementsByClassName("gridCell");
 
-    gridContainer.style.gridTemplateColumns = `repeat(${gSize}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${gSize}, 1fr )`;
 
-    for(let i =0; i < gridSize * gridSize; i++){
+function makeGrid(gridSize) {
+
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr )`;
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
         createDiv = document.createElement('div');
-        createDiv.classList.add('cell');
+        createDiv.classList.add('gridCell');
+        createDiv.addEventListener("mouseover", function (e) {
+            changeColor(e.target);
+        })
         gridContainer.appendChild(createDiv);
     }
-
+    return;
 }
-function resetGrid(){
 
-}
-function colorPickerGrid(){
+function resetGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    let prompt = window.prompt("reset");
+    gridMaker = makeGrid(prompt);
 
+    return;
 }
-function randomColorGrid(){
+function changeColor(divTarget){
+    divTarget.style.backgroundColor = "#000";
+    return;
+}
 
+//ability to choose color from hexcodes
+function chooseColor() {
+    let cellR = document.querySelectorAll(".gridCell");
+    let color = window.prompt("input a color");
+    cellR.forEach(gridCells => {
+      gridCells.addEventListener("mouseover", e => {
+        e.target.style.backgroundColor = color;
+      });
+    });
 }
+
+function random() {
+    let hash = "#";
+    let colour = Math.floor(Math.random() * 16777215).toString(16);
+    return hash + colour;
+  }
+function rainbow() {
+    // let colour = Math.floor(Math.random() * 16777215).toString(16);
+    let cellR = document.querySelectorAll(".gridCell");
+    cellR.forEach(gridCells => {
+      gridCells.addEventListener("mouseover", e => {
+        e.target.style.backgroundColor = random();
+      });
+    });
+}
+
+//handles button clicks
+document.getElementById('reset').addEventListener("click", resetGrid, false);
+document.getElementById('random').addEventListener("click", rainbow, false);
+document.getElementById('colorpicker').addEventListener("click", chooseColor, false);
+
